680130940a42c855e3412d914612885b97994719
Leendert Hayen <leendert.hayen@kuleuven.be>
2019-10-03 14:07:17 +0200

    Merge conflicts resolved & minor tests change

--- tests/test_generator.cc
+++ tests/test_generator.cc
@@ -15,4 +15,16 @@ TEST_CASE("Basic construction") {
   REQUIRE(bp.Zi == 20);
   REQUIRE(bp.A == 45);
   REQUIRE(bp.Zf == 21);
+  REQUIRE(bp.betaType == 1);
+}
+
+TEST_CASE("Spectrum calculation") {
+  std::string iniFile = "/home/leendert/git/BSG/tests/45Ca.ini";
+  std::string configFile = "";
+  BSG::Generator gen;
+
+  bool success = gen.InitializeTransitionFromFile(iniFile);
+  REQUIRE(success == true);
+
+
 }
